08b92a8dbdc635fb95b5acaa2446b619c072cceb
Jenny Lian <jennylian100@gmail.com>
2018-08-19 16:29:40 -0700

    made the dictionary

--- src/com/jennylia/Main.java
+++ src/com/jennylia/Main.java
@@ -1,11 +1,8 @@
 package com.jennylia;
 
-import com.jennylia.libraries.sentenceTranslate;
 import com.jennylia.libraries.utils.DataPopulator;
 import com.jennylia.libraries.utils.MapPrinter;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
@@ -13,7 +10,6 @@ public class Main {
   public static void main(String[] args) {
     DataPopulator dataPopulator = new DataPopulator();
     Map<String, String> corporateDictionary = dataPopulator.getDictionary();
-
     MapPrinter.printMap(corporateDictionary);
 
   }
--- src/com/jennylia/libraries/utils/DataPopulator.java
+++ src/com/jennylia/libraries/utils/DataPopulator.java
@@ -17,10 +17,16 @@ public class DataPopulator {
     Map<String, String> dictionary = new HashMap<>();
     for (int i = 0; i < parsedInput.length; i++) {
       // the key
-      dictionary.put(parsedInput[i].trim(), parsedInput[++i].trim());
+      dictionary.put(trimString(parsedInput[i]), trimString(parsedInput[++i]));
     }
 
     return dictionary;
+  }
+
+  private String trimString(String input) {
+    String pattern = "(?m)^\\s*\\r?\\n|\\r?\\n\\s*(?!.*\\r?\\n)";
+    String replacement = "";
+    return input.replaceAll(pattern,replacement).trim();
   }
 
   public Map<String, String> getDictionary() {
